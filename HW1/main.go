11be2cf7478500fb1e2b4965e69f99b2e8c6a9f6
Elahe Dastan <elahe.dstn@gmail.com>
2020-08-14 23:43:33 +0430

    Read data from csv file

--- HW1/main.go
+++ HW1/main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"gonum.org/v1/plot"
+)
+
 func main() {
-	$END$
+
+}
+
+// I want to read the data from dataset and plot it
+func show() {
+	f, err := os.Open("dataset.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	dataAsBytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Println(err)
+	}
+
+	dataAsText := string(dataAsBytes)
+
+	points := strings.Split(dataAsText, "\n")[1:]
+
+	p, err := plot.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p.Title.Text = "ML dataset"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+
 }
