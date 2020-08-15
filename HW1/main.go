95735ab329f1f7a14d8376080649dbe65d2d1236
Elahe Dastan <elahe.dstn@gmail.com>
2020-08-15 12:29:53 +0430

    Plot the data

--- HW1/main.go
+++ HW1/main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	"gonum.org/v1/plot"
 )
 
 func main() {
-
+	show()
 }
 
 // I want to read the data from dataset and plot it
 func show() {
-	f, err := os.Open("dataset.csv")
+	f, err := os.Open("/home/raha/go/src/ML/HW1/dataset.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,4 +43,29 @@ func show() {
 	p.X.Label.Text = "X"
 	p.Y.Label.Text = "Y"
 
+	pts := reformatPoints(points)
+
+	s, err := plotter.NewScatter(pts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p.Add(s)
+	p.Legend.Add("scatter", s)
+
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "points.png"); err != nil {
+		panic(err)
+	}
+}
+
+func reformatPoints(points []string) plotter.XYs {
+	pts := make(plotter.XYs, len(points))
+	for i := range points {
+		XY := strings.Split(points[i], ",")
+
+		pts[i].X, _ = strconv.ParseFloat(XY[0], 64)
+		pts[i].Y, _ = strconv.ParseFloat(XY[1], 64)
+	}
+
+	return pts
 }
