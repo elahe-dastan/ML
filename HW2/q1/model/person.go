19b37e8b50c2322821548bfe77552e945a147bc0
Elahe Dastan <elahe.dstn@gmail.com>
2020-08-20 23:07:49 +0430

    Add model

--- HW2/q1/model/person.go
+++ HW2/q1/model/person.go
@@ -1,1 +1,9 @@
 package model
+
+trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal,num
+145.0,233.0,1.0,2.0,150.0,0.0,2.3,3.0,0.0,6.0,0
+type Person struct {
+	Age float64
+	Sex float64
+	CP float64
+}
